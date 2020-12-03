7a360b7debc6b25e76984b59b14f45180152adfb
DESKTOP-1176HB0\zlshen <1129120844@qq.com>
2020-12-03 18:30:41 +0800

    Initial commit

---
+++ src/main/java/com/testboot/TestbootApplication.java
@@ -0,0 +1,13 @@
+package com.testboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/testboot/test/Test.java
@@ -0,0 +1,13 @@
+package com.testboot.test;
+
+/**
+ * @author zlshen
+ * @create 2020-12-03 18:27
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("123456");
+    }
+
+}
